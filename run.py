efcae1f61e0f47f938c232a3742b2583ba229bc8
Marc Villain <marc.villain@epita.fr>
2020-08-07 04:26:32 +0200

    feat(run): get channel id based on channel name

--- run.py
+++ run.py
@@ -11,3 +11,13 @@ channel_name = sys.argv[2]
 word_to_extract = sys.argv[3]
 
 api = YoutubeAPI(api_key)
+
+print("> Get channel ID")
+search_results = api.search(
+    max_results_count=1,
+    q=channel_name,
+    part="snippet,id",
+    type="channel",
+    maxResults="1",
+)
+channel_id = search_results[0]["id"]["channelId"]
